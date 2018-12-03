0e0f2013104359d4034a7b8db7d80247947de406
vivekchalana <vivekchalana@yahoo.com>
2018-12-03 12:34:00 -0800

    grid transition

---
+++ Sample Web Site/js/main.js
@@ -0,0 +1,12 @@
+document.getElementsByClassName("icon")[0].onclick = function(){
+
+	document.getElementsByTagName("li")[0].style.display= "block";
+	document.getElementsByTagName("li")[1].style.display= "block";
+	document.getElementsByTagName("li")[2].style.display= "block";
+	document.getElementsByTagName("li")[3].style.display= "block";
+
+}
+
+
+
+
